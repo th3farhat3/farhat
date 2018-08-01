d06fe3ac07678b93db110ce097866af3370c376e
Mohamed Farhat <37718820+th3farhat@users.noreply.github.com>
2018-08-01 20:33:05 +0200

    Delete admin1.js

--- farhat-blogger/admin1.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-    function highlight() {
-        $('.user.blog-author,.ssyby').closest('.comment-block').css('background', 'linear-gradient(50deg,#ffffff,#ffffff)').css('padding', '0');
-    }
-    $(document).bind('ready scroll click', highlight);
-});
