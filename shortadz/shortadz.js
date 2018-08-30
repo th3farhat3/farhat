1752d31712101e1d4253435fe906cef0c8ece4ee
Mohamed Farhat <37718820+th3farhat@users.noreply.github.com>
2018-08-30 05:25:48 +0200

    Create shortadz.js

---
+++ shortadz/shortadz.js
@@ -0,0 +1,4 @@
+ var shortadz_url = &#39;https://shortadz.com/&#39;;
+            var shortadz_api_token = &#39;99de3e93c1d4236f259a122b3b64e4310072df28&#39;;
+            var shortadz_advert = 2;
+            var shortadz_exclude_domains = [&#39;www.th3farhat.com&#39;, &#39;bit.ly&#39;];
