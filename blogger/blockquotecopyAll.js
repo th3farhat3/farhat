191ba5fda53381b568561b3222e93cf8948ba5cb
Mohamed Farhat <37718820+th3farhat@users.noreply.github.com>
2018-08-30 05:21:32 +0200

    Create blockquotecopyAll.js

---
+++ blogger/blockquotecopyAll.js
@@ -0,0 +1,2 @@
+;for(var 
+pres=document.querySelectorAll("pre,kbd,blockquote,i"),i=0;i<pres.length;i++)pres[i].addEventListener("dblclick",function(){var e=getSelection(),t=document.createRange();t.selectNodeContents(this),e.removeAllRanges(),e.addRange(t)},!1);
