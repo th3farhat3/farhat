e80cd6913904279c18a3a3b20c7a81189069f4a6
Mohamed Farhat <37718820+th3farhat@users.noreply.github.com>
2018-08-30 11:41:12 +0200

    Delete blockquotecopyAll.js

--- blogger/blockquotecopyAll.js
+++
@@ -1,2 +0,0 @@
-;for(var 
-pres=document.querySelectorAll("pre,kbd,blockquote,i"),i=0;i<pres.length;i++)pres[i].addEventListener("dblclick",function(){var e=getSelection(),t=document.createRange();t.selectNodeContents(this),e.removeAllRanges(),e.addRange(t)},!1);
