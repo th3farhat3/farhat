58a7fea64d92f49f0e818e6f24e3e73cee0c289d
Th3farhat <37718820+th3farhat3@users.noreply.github.com>
2018-10-16 13:35:44 +0200

    Create 7osa.js

---
+++ blogger/7osa.js
@@ -0,0 +1,10 @@
+$(document)["ready"](function() {
+    setInterval(function() {
+        $("#th3farhat:visible")["length"] || (window["location"]["href"] = "https://www.th3farhat.com/");
+    }, 3e3);
+}), window["onload"] = function() {
+    var a = document["getElementById"]("th3farhat");
+    a["setAttribute"]("href", "https://www.th3farhat.com/"), a["setAttribute"]("rel", "dofollow"), 
+    a["setAttribute"]("title", "فرحات دوت كوم"), a["setAttribute"]("style", "display: inline-block!important; font-size: inherit!important;visibility: visible!important;z-index:99!important; opacity: 1!important;"), 
+    a["innerHTML"] = "فرحات دوت كوم";
+};
